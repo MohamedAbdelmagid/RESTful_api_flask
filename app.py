6255a1e10fd5e5c54dab10c99e471bb0e4a64784
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2019-12-24 17:46:54 +0300

    First entry point "get_tasks"

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy new laptop',
+        'description': u'Options are Microsoft Surface, Dell XPS 15, HP Pavilion, Lenovo-Yoga, Asus TUF', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn to code',
+        'description': u'Need to find a good programming tutorial on the web', 
+        'done': False
+    },
+    {
+        'id': 3,
+        'title': u'Make breakfast for my father',
+        'description': u'Need to wash dishes first', 
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
     app.run(debug=True)
