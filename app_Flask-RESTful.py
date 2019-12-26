117004e42f17cfe37173c6b03410be0b75aa6fd2
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2019-12-26 16:00:18 +0300

    Add request parsers & arguments restrictions

--- app_Flask-RESTful.py
+++ app_Flask-RESTful.py
@@ -1,11 +1,19 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
 class TasksAPI(Resource):
-    
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+
+        self.reqparse.add_argument('title', type=str, required=True, help='No task title provided', location='json')
+        self.reqparse.add_argument('description', type=str, default="", location='json')
+
+        super(TasksAPI, self).__init__()
+
     def get(self):
         pass
 
@@ -13,6 +21,15 @@ class TasksAPI(Resource):
         pass
 
 class TaskAPI(Resource):
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+
+        self.reqparse.add_argument('title', type=str,  location='json')
+        self.reqparse.add_argument('description', type=str, location='json')
+        self.reqparse.add_argument('done', type=bool, location='json')
+
+        super(TaskAPI, self).__init__()
 
     def get(self, id):
         pass
