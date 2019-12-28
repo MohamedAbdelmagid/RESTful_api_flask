ccf65b381a92f2f2a8c444b0c0ddafbda17f5130
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2019-12-28 08:24:11 +0300

    Add list of tasks & implement the funtions

--- app_Flask-RESTful.py
+++ app_Flask-RESTful.py
@@ -1,8 +1,38 @@
-from flask import Flask, request
+from flask import Flask, request, url_for, abort
 from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy new laptop',
+        'description': u'Options are Microsoft Surface, Dell XPS 15, HP Pavilion, Lenovo-Yoga, Asus TUF', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn to code',
+        'description': u'Need to find a good programming tutorial on the web', 
+        'done': False
+    },
+    {
+        'id': 3,
+        'title': u'Make breakfast for my father',
+        'description': u'Need to wash dishes first', 
+        'done': False
+    }
+]
+
+# This funtion return a task with specific id from the tasks list
+def get_task_with_id(id):
+    task = filter(lambda task: task['id'] == id, tasks)
+    if len(task) == 0:
+        abort(404)
+    task = task[0]  # because filter fun get you a list of one element
+    
+    return task
 
 class TasksAPI(Resource):
 
@@ -15,10 +45,19 @@ class TasksAPI(Resource):
         super(TasksAPI, self).__init__()
 
     def get(self):
-        pass
+        return {'task': [task for task in tasks]}
 
     def post(self):
-        pass
+        args = self.reqparse.parse_args()
+        task = {
+            'id': tasks[-1]['id'] + 1 if len(tasks) > 0 else 1,
+            'title': args['title'],
+            'description': args['description'],
+            'done': False
+        }
+        tasks.append(task)
+
+        return {'task': task}, 201
 
 class TaskAPI(Resource):
 
@@ -32,13 +71,25 @@ class TaskAPI(Resource):
         super(TaskAPI, self).__init__()
 
     def get(self, id):
-        pass
+        task = get_task_with_id(id)
+        return {'task': task}
 
     def put(self, id):
-        pass
+        task = get_task_with_id(id)
+
+        args = self.reqparse.parse_args()
+        for key, value in args.iteritems():
+            if value != None:
+                task[key] = value
+
+        return {'task': task}
 
     def delete(self, id):
-        pass
+        task = get_task_with_id(id)
+        tasks.remove(task)
+        print(tasks)
+
+        return {'Removed': True, 'Task': task['title']}
 
 api.add_resource(TasksAPI, '/todo/api/v1.0/tasks', endpoint='tasks')
 api.add_resource(TaskAPI, '/todo/api/v1.0/tasks/<int:id>', endpoint='task')
